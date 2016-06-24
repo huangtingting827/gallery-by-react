8f6cba4c3e6154101b90d0ba3164f36e606f3a8c
huangtingting <huangtingting827@sina.com>
2016-06-24 21:39:42 +0800

    重新上传

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -47,7 +47,7 @@ function getDefaultModules() {
 }
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/gallery-by-react/assets/',
+  publicPath: '/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules,
   postcss: function () {
